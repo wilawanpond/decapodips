56a34069ca11fe4cbc28688dae9e3e039fd7359d
KeyClover <suvichak0001@gmail.com>
2024-02-14 23:46:16 +0700

    Adding intent from SavedSauce to Main/UrSauce/Favorite/Account.

--- app/src/main/java/com/example/decapodips/SavedSauce.kt
+++ app/src/main/java/com/example/decapodips/SavedSauce.kt
@@ -1,7 +1,10 @@
 package com.example.decapodips
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ListView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.decapodips.adapters.SpecialAdapter
@@ -16,14 +19,47 @@ class SavedSauce : AppCompatActivity() {
         setContentView(R.layout.activity_saved_sauce)
 
         init()
+
+        val toHome = findViewById<ImageView>(R.id.homeBtn)
+        toHome.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        // to Account page
+        val toAccount = findViewById<ImageView>(R.id.accountBtn)
+        toAccount.setOnClickListener {
+            var intent = Intent(this, Account::class.java)
+            startActivity(intent)
+        }
+
+        //to Favorite page
+        val toFavorite = findViewById<ImageView>(R.id.favoriteBtn)
+        toFavorite.setOnClickListener {
+            var intent = Intent(this, Favorite::class.java)
+            startActivity(intent)
+        }
+
+
+        // to CustomizedSauce Page
+        val toUrSauce = findViewById<ImageView>(R.id.sauceBtn)
+        toUrSauce.setOnClickListener{
+            var intent = Intent(this, CustomizedSauce::class.java)
+            startActivity(intent)
+        }
+
     }
 
     private fun init() {
         recyclerView = findViewById(R.id.savedSauceRecycler)
         recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = layoutManager
 
         specialAdapter = SpecialAdapter(StoredSpecial.special)
         recyclerView.adapter = specialAdapter
     }
+
+
 }
