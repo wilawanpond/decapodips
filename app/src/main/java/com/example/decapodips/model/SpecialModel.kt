8fc52111b62ca598a93544e60d5d62065db8b062
Wilawan Pond <79263616+wilawanpond@users.noreply.github.com>
2024-02-15 00:04:25 +0700

    Added more items on saved sauce page

--- app/src/main/java/com/example/decapodips/model/SpecialModel.kt
+++ app/src/main/java/com/example/decapodips/model/SpecialModel.kt
@@ -8,6 +8,9 @@ object StoredSpecial {
     val special = arrayListOf(
         SpecialModel(R.drawable.sauce6, "Special no.1", "Crab, Chinese, Medium"),
         SpecialModel(R.drawable.sauce5, "Special no.2", "Crab, Japanese, Medium"),
-        SpecialModel(R.drawable.sauce4, "Special no.3", "Shrimp, French, Hot!")
+        SpecialModel(R.drawable.sauce4, "Special no.3", "Shrimp, French, Hot!"),
+        SpecialModel(R.drawable.sauce1, "Special no.4", "Shrimp, Japanese, Mild"),
+        SpecialModel(R.drawable.sauce2, "Special no.5", "Crab, Chinese, Original"),
+        SpecialModel(R.drawable.sauce3, "Special no.6", "Crab, French, Mild")
     )
 }
