71364fc3d95b8b90032ccd9cb5a0270ac3249296
Wilawan Pond <79263616+wilawanpond@users.noreply.github.com>
2024-02-14 23:50:13 +0700

    final recyclerView for saved sauce!

--- app/src/main/java/com/example/decapodips/model/SpecialModel.kt
+++ app/src/main/java/com/example/decapodips/model/SpecialModel.kt
@@ -6,8 +6,8 @@ data class SpecialModel(val image: Int, val name: String, val description: Strin
 
 object StoredSpecial {
     val special = arrayListOf(
-        SpecialModel(R.drawable.sauce6, "Special no.1", "Crab, Pepper, Chinese, Medium"),
-        SpecialModel(R.drawable.sauce5, "Special no.2", "Crab, Chili, Japanese, Medium"),
-        SpecialModel(R.drawable.sauce4, "Special no.3", "Shrimp, Ginger, French, Hot!")
+        SpecialModel(R.drawable.sauce6, "Special no.1", "Crab, Chinese, Medium"),
+        SpecialModel(R.drawable.sauce5, "Special no.2", "Crab, Japanese, Medium"),
+        SpecialModel(R.drawable.sauce4, "Special no.3", "Shrimp, French, Hot!")
     )
 }
