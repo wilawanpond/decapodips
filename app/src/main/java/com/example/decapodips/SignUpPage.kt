4aa0a30253803389bf8ab51af90407d5c8f2453c
Wilawan Pond <79263616+wilawanpond@users.noreply.github.com>
2024-02-15 01:16:00 +0700

    Update other options for sign up

--- app/src/main/java/com/example/decapodips/SignUpPage.kt
+++ app/src/main/java/com/example/decapodips/SignUpPage.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.content.Intent
+import android.net.Uri
 import android.widget.Button
+import android.widget.ImageView
 
 class SignUpPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +28,35 @@ class SignUpPage : AppCompatActivity() {
             var intent = Intent(this, EbiKaniWelcome::class.java)
             startActivity(intent)
         }
+
+        // other options for sign up
+        // another sign in options (facebook)
+        val facebookBtn = findViewById<ImageView>(R.id.facebookBtn)
+        facebookBtn.setOnClickListener {
+            val url = "https://www.facebook.com/"
+            val facebook = Intent(Intent.ACTION_VIEW)
+            facebook.setData(Uri.parse(url))
+            startActivity(facebook)
+        }
+
+        // instagram
+        val instagramBtn = findViewById<ImageView>(R.id.instagramBtn)
+        instagramBtn.setOnClickListener {
+            val url = "https://www.instagram.com/"
+            val instagram = Intent(Intent.ACTION_VIEW)
+            instagram.setData((Uri.parse(url)))
+            startActivity(instagram)
+        }
+
+        // email
+        val emailBtn = findViewById<ImageView>(R.id.emailBtn)
+        emailBtn.setOnClickListener {
+            val url = "https://mail.google.com/"
+            val email = Intent(Intent.ACTION_VIEW)
+            email.setData(Uri.parse(url))
+            startActivity(email)
+        }
+
 
     }
 }
