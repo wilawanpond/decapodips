e09398edcc86a825b76e6c435396113107eae78f
KeyClover <suvichak0001@gmail.com>
2024-02-15 13:52:42 +0700

    adding toast for mild/original/medium/hot on CustomizeadSauce

--- app/src/main/java/com/example/decapodips/CustomizedSauce.kt
+++ app/src/main/java/com/example/decapodips/CustomizedSauce.kt
@@ -40,52 +40,84 @@ class CustomizedSauce : AppCompatActivity() {
             startActivity(intent)
         }
 
+        //toast carb
         val crabBtn = findViewById<Button>(R.id.crabBtn)
         crabBtn.setOnClickListener{
             Toast.makeText(this, crabBtn.text , Toast.LENGTH_SHORT).show()
         }
 
+        //toast shrimp
         val shrimpBtn = findViewById<Button>(R.id.shrimpBtn)
         shrimpBtn.setOnClickListener{
             Toast.makeText(this, shrimpBtn.text, Toast.LENGTH_SHORT).show()
         }
-
+        //toast no meat
         val noMeatBtn = findViewById<Button>(R.id.noMeatBtn)
         noMeatBtn.setOnClickListener{
            Toast.makeText(this, noMeatBtn.text, Toast.LENGTH_SHORT).show()
         }
 
+        //toast garlic
         val garlicBtn = findViewById<Button>(R.id.garlicBtn)
         garlicBtn.setOnClickListener{
             Toast.makeText(this, garlicBtn.text, Toast.LENGTH_SHORT).show()
         }
 
+        //toast chili
         val chiliBtn = findViewById<Button>(R.id.chiliBtn)
         chiliBtn.setOnClickListener{
             Toast.makeText(this, chiliBtn.text, Toast.LENGTH_SHORT).show()
         }
 
+        //toast coriander
         val corianderBtn = findViewById<Button>(R.id.corianderBtn)
         corianderBtn.setOnClickListener{
             Toast.makeText(this, corianderBtn.text, Toast.LENGTH_SHORT).show()
         }
 
+
+        //toast eschalot
         val eschalotBtn = findViewById<Button>(R.id.eschalotBtn)
         eschalotBtn.setOnClickListener{
             Toast.makeText(this, eschalotBtn.text, Toast.LENGTH_SHORT).show()
         }
 
+        //toast ginger
         val gingerBtn = findViewById<Button>(R.id.gingerBtn)
         gingerBtn.setOnClickListener{
             Toast.makeText(this, gingerBtn.text, Toast.LENGTH_SHORT).show()
         }
 
+        //toast btn
         val pepperBtn = findViewById<Button>(R.id.pepperBtn)
         pepperBtn.setOnClickListener{
             Toast.makeText(this, pepperBtn.text, Toast.LENGTH_SHORT).show()
         }
 
+        //toast mild
+        val mildBtn = findViewById<Button>(R.id.mildBtn)
+        mildBtn.setOnClickListener{
+            Toast.makeText(this, mildBtn.text, Toast.LENGTH_SHORT).show()
+        }
 
+        //toast original
+        val originalBtn = findViewById<Button>(R.id.originalBtn)
+        originalBtn.setOnClickListener{
+            Toast.makeText(this, originalBtn.text, Toast.LENGTH_SHORT).show()
+        }
+
+        //toast medium
+        val mediumBtn = findViewById<Button>(R.id.mediumBtn)
+        mediumBtn.setOnClickListener{
+            Toast.makeText(this, mediumBtn.text, Toast.LENGTH_SHORT).show()
+        }
+
+
+        //toast hot
+        val hotBtn = findViewById<Button>(R.id.hotBtn)
+        hotBtn.setOnClickListener{
+            Toast.makeText(this, hotBtn.text, Toast.LENGTH_SHORT).show()
+        }
 
 
 
